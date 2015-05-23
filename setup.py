bd359d4f27713df6bda8ad0e559788e02c0e6d2c
Parv Jain <parv.wutizup@gmail.com>
2015-05-24 01:00:09 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import requests, hashlib
+
+email = raw_input('Enter your email address: ');
+
+response = requests.get("https://en.gravatar.com/" + hashlib.md5(email.lower()).hexdigest() + ".json") 
+
+fo = open('assets/js/main.js', 'w')
+fo.write("""
+	var source = document.getElementById("myTemplate").innerHTML;
+var template = Handlebars.compile(source);
+var data = """+ response.text +"""
+
+document.getElementById("placeholder").innerHTML = template(data);
+	""")
+fo.close
